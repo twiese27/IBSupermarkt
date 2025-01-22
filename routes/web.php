1fb2575aaeb0ec38be5ad88710ce3370fd9fd73b
Lars Uphoff <lars.uphoff1@gmail.com>
2025-01-22 13:18:13 +0100

    Changed html suffix to blade-php and added routes

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,38 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\HomePageController;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
 //Route::get('/', [HomePageController::class, 'index']);
 
+Route::get('/', function () {
+    return view('index');
+});
 
 Route::get('/cart', function () {
     return view('cart');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/shop-grid', function () {
+    return view('shop-grid');
+});
+
+Route::get('/shop-single', function () {
+    return view('shop-single');
 });
 
 Route::get('/controller', [HomePageController::class, 'index']);
