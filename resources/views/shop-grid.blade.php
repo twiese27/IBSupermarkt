7f6d9f4913fa3053471ef5ab524a12103e37fdfa
Lennart Schwartz <lennart.schwartz@student.ibs-ol.de>
2025-02-05 16:12:24 +0100

    Quick fix: Pagination optisch ansprechend machen, damit der Lars auch zufrieden ist

--- resources/views/shop-grid.blade.php
+++ resources/views/shop-grid.blade.php
@@ -388,8 +388,12 @@
       </div>
 
       <!-- Pagination-Links -->
-      <div class="pagination-container mt-4">
-        {{ $products->appends(['search' => request()->search])->links() }}
+      <div class="pagination-container mt-4 d-flex justify-content-center">
+        <nav>
+          <ul class="pagination d-flex flex-wrap">
+              {{ $products->links('pagination::bootstrap-4') }}
+          </ul>
+        </nav>
       </div>
     </div>
   </div>
